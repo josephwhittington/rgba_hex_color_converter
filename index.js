58ef0df1f94810ef1a6aaad186b0936f0e2b1ac7
josephwhittington <joseph.a.whittington@gmail.com>
2019-11-01 00:01:25 -0400

    bug fixes

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ document.getElementById("button").addEventListener("click", (e) =>
     let hex = document.getElementById("hex").checked
     let color = document.getElementById("color").value;
 
+    let error = false;
 
     if(hex && color.trim().length > 0)
     {
         color = parseInt(color, 16);
     } else if(!hex && color.trim().length > 0){
         color = parseInt(color, 10); 
+        if(color === 0 && document.getElementById("color").value.search(/[xX]/))
+        {
+            error = true;
+            confirm = confirm("It appears that you input a hex value but didnt check the hex box. Did you want to input a hex color?")
+
+            if(confirm === true)
+            {
+                error = false;
+                color = parseInt(document.getElementById("color").value, 16);
+            }
+        }
     }
+    console.log(color)
 
     const alpha = (color & 0xFF000000) >>> 24;
     const red = (color & 0x00FF0000) >>> 16;
@@ -26,6 +39,8 @@ document.getElementById("button").addEventListener("click", (e) =>
                 <li class="list-group-item">Blue: ${blue} (${(blue/255).toFixed(2)})</li>
             </ul>
         `
-
+    if(!error)
+    {
         document.getElementById("output").innerHTML = output;
+    }
 })
